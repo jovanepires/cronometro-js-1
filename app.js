67a40791c56b1c2a6172eeda2bd5742fee1067c8
David Alves <dev.david@aticonsultoria.com>
2016-08-31 16:39:30 -0300

    Correções

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-var second = 0;
 var minute = 0;
+var second = 0;
 var cron;
 
 document.getElementById('start').addEventListener('click', function() {
 	document.getElementById('start').disabled = true;
-	document.getElementById('clear').disabled = true;
+	document.getElementById('reload').disabled = true;
 	cron = setInterval(function() {
 		second++;
-  		document.getElementById("timer").innerHTML = parseInt(second).toFixed(0);
-	}, 1000);
+		if (second > 59) {
+			minute++;
+			document.getElementById('minute').innerHTML = parseInt(minute).toFixed(0) > 9 ? parseInt(minute).toFixed(0) : '0' + parseInt(minute).toFixed(0);
+			second = 0;
+		}
+
+  		document.getElementById('second').innerHTML = parseInt(second).toFixed(0) > 9 ? parseInt(second).toFixed(0) : '0' + parseInt(second).toFixed(0);
+
+	}, 100);
 });
 
-document.getElementById('stop').addEventListener('click', function(){
+document.getElementById('pause').addEventListener('click', function(){
 	clearInterval(cron);
-		document.getElementById('start').disabled = false;
-		document.getElementById('clear').disabled = false;
+	document.getElementById('start').disabled = false;
+	document.getElementById('reload').disabled = false;
 });
 
-document.getElementById('clear').addEventListener('click', function(){
+document.getElementById('reload').addEventListener('click', function(){
+	minute = 0;
 	second = 0;
-	document.getElementById("timer").innerHTML = 0;
+	document.getElementById('second').innerHTML = '00';
+	document.getElementById('minute').innerHTML = '00';
 });
