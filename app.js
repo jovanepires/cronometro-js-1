6b61041ee17ce24e12f92ff0d7bb4be773719541
davidalves1 <alves.david@outlook.com>
2016-08-31 22:21:55 -0300

    Implementar milissegundos

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 var minute = 0;
 var second = 0;
+var millisecond = 0;
 var cron;
 
 document.getElementById('start').addEventListener('click', function() {
 	document.getElementById('start').disabled = true;
 	document.getElementById('reload').disabled = true;
 	cron = setInterval(function() {
-		second++;
+		millisecond++;
+
+		if (millisecond > 9) {
+			second++;
+			document.getElementById('second').innerHTML = parseInt(second).toFixed(0) > 9 ? parseInt(second).toFixed(0) : '0' + parseInt(second).toFixed(0);
+			millisecond = 0;
+		}
+
 		if (second > 59) {
 			minute++;
 			document.getElementById('minute').innerHTML = parseInt(minute).toFixed(0) > 9 ? parseInt(minute).toFixed(0) : '0' + parseInt(minute).toFixed(0);
 			second = 0;
 		}
 
-  		document.getElementById('second').innerHTML = parseInt(second).toFixed(0) > 9 ? parseInt(second).toFixed(0) : '0' + parseInt(second).toFixed(0);
+  		document.getElementById('millisecond').innerHTML = parseInt(millisecond).toFixed(0);
 
 	}, 100);
 });
@@ -27,6 +35,7 @@ document.getElementById('pause').addEventListener('click', function(){
 document.getElementById('reload').addEventListener('click', function(){
 	minute = 0;
 	second = 0;
+	document.getElementById('millisecond').innerHTML = '0';
 	document.getElementById('second').innerHTML = '00';
 	document.getElementById('minute').innerHTML = '00';
 });
